b82ceb819f4600f8ee9f207bbd7cc9d24ac55411
Zenocolo <phzenhae@microsoft.com>
2021-09-13 23:32:35 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
+variable "project_name" {
+  description = "Name of project"
+}
 
+variable "vmcount" {
+  description = "Number of objects to be created"
+}
+
+variable "location" {
+  description = "Location of the Resource Group"
+}
+
+variable "rg-name" {
+  description = "Resource Group name"
+}
+
+variable "virtual_machine_ids" {
+  description = "Virtual Machine ID's to install the Monitoring Agent"
+}
